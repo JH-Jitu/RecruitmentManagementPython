ef8af99821d67b44fcb405b908c7c03da1dab827
JH-Jitu <jitu000166@gmail.com>
2023-11-11 19:04:44 +0600

    template of login is created

---
+++ Login.py
@@ -0,0 +1,100 @@
+import os
+
+def execute():
+    while True:
+        print("\n### Login ###")
+        print("1. Developer")
+        print("2. Recruiter")
+        print("3. Company")
+        print("4. Admin")
+        print("5. Back to Main Menu")
+        print("6. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            login_as_developer()
+        elif choice == "2":
+            login_as_recruiter()
+        elif choice == "3":
+            login_as_company()
+        elif choice == "4":
+            login_as_admin()
+        elif choice == "5":
+            break
+        elif choice == "6":
+            exit()
+        else:
+            print("Invalid choice. Please try again.")
+
+def login_as_developer():
+    email = input("Enter developer email: ")
+    password = input("Enter developer password: ")
+
+    # Validate developer credentials
+    if validate_credentials("DB\\RegisteredDevelopers.txt", email, password):
+        # Call developer panel
+        execute_developer(email)
+    else:
+        print("Invalid credentials. Please try again.")
+
+def login_as_recruiter():
+    email = input("Enter recruiter email: ")
+    password = input("Enter recruiter password: ")
+
+    # Validate recruiter credentials
+    if validate_credentials("DB\\RegisteredRecruiters.txt", email, password):
+        # Call recruiter panel
+        execute_recruiter(email)
+    else:
+        print("Invalid credentials. Please try again.")
+
+def login_as_company():
+    email = input("Enter company email: ")
+    password = input("Enter company password: ")
+
+    # Validate company credentials
+    if validate_credentials("DB\\RegisteredCompanies.txt", email, password):
+        # Call company panel
+        execute_company(email)
+    else:
+        print("Invalid credentials. Please try again.")
+
+def login_as_admin():
+    email = input("Enter admin email: ")
+    password = input("Enter admin password: ")
+
+    # Validate admin credentials
+    if validate_credentials("DB\\Admins.txt", email, password):
+        # Call admin panel
+        execute_admin(email)
+    else:
+        print("Invalid credentials. Please try again.")
+
+def validate_credentials(file_path, email, password):
+    # Validate credentials from the specified file
+    with open(file_path, "r") as file:
+        for line in file:
+            stored_email, stored_password, _ = line.strip().split("#")
+            if email == stored_email and password == stored_password:
+                return True
+    return False
+
+def execute_developer(developer_email):
+    # Call the developer panel
+    os.system("python Developer.py")
+
+def execute_recruiter(recruiter_email):
+    # Call the recruiter panel
+    os.system("python Recruiter.py")
+
+def execute_company(company_email):
+    # Call the company panel
+    os.system("python Company.py")
+
+def execute_admin(admin_email):
+    # Call the admin panel
+    os.system("python Admin.py")
+
+if __name__ == "__main__":
+    execute()
