af9f52500743a7ff5ec1a58b2f86abdd0dc2b99c
JH-Jitu <jitu000166@gmail.com>
2023-11-11 19:15:18 +0600

    template of Execute is created

--- Execute.py
+++ Execute.py
@@ -1,1 +1,28 @@
+import os
+from Login import execute as execute_login
+from Registration import execute as execute_registration
+from Utility import execute as execute_utility
+
 print("Hello")
+
+def execute():
+    while True:
+        print("\n### Press any option and hit enter ###")
+        print("1. Login")
+        print("2. Registration")
+        print("3. Exit")
+
+        option = input("Enter your choice: ")
+
+        if option == "1":
+            execute_login()
+        elif option == "2":
+            execute_registration()
+        elif option == "3":
+            exit()
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    execute_utility()  # Ensure the 'DB' folder and files are created
+    execute()
