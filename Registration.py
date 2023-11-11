fdb665ddca73ff516f68d4e049a6c9dfcf28b458
JH-Jitu <jitu000166@gmail.com>
2023-11-11 19:14:03 +0600

    template of registration is updated and utlity is created

--- Registration.py
+++ Registration.py
@@ -1,12 +1,15 @@
 import os
+import random
+import string
 
-def register_user():
+def execute():
     while True:
         print("\n### Registration ###")
-        print("1. Developer")
-        print("2. Company")
-        print("3. Back to Main Menu")
-        print("4. Exit")
+        print("1. Register as Developer")
+        print("2. Register as Company")
+        print("3. Register as Recruiter")
+        print("4. Back to Main Menu")
+        print("5. Exit")
 
         choice = input("Enter your choice: ")
 
@@ -15,33 +18,41 @@ def register_user():
         elif choice == "2":
             register_company()
         elif choice == "3":
-            break
+            register_recruiter()
         elif choice == "4":
+            break
+        elif choice == "5":
             exit()
         else:
             print("Invalid choice. Please try again.")
 
+def generate_random_password():
+    # Generate a random password of length 8
+    password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    return password
+
 def register_developer():
-    print("\n### Developer Registration ###")
+    print("\n### Register as Developer ###")
     email = input("Enter your email: ")
-    password = input("Enter your password: ")
+    password = generate_random_password()
     name = input("Enter your name: ")
     age = input("Enter your age: ")
-    expertise_skills = input("Enter your expertise skills: ")
-    project_links = input("Enter your project links: ")
+    expertise_skills = input("Enter your expertise skills (comma-separated): ")
+    project_links = input("Enter your project links (comma-separated): ")
     linkedin_link = input("Enter your LinkedIn link: ")
 
     # Save developer information to file (RegisteredDevelopers.txt)
     with open("DB\\RegisteredDevelopers.txt", "a") as file:
         file.write(f"{email}#{password}#{name}#{age}#{expertise_skills}#{project_links}#{linkedin_link}\n")
 
-    print("Developer registered successfully!")
+    print("Registration successful!")
+    print(f"Your password is: {password}")
 
 def register_company():
-    print("\n### Company Registration ###")
+    print("\n### Register as Company ###")
     name = input("Enter company name: ")
-    email = input("Enter your email: ")
-    password = input("Enter your password: ")
+    email = input("Enter company email: ")
+    password = generate_random_password()
     license_no = input("Enter company license number: ")
     employee_number = input("Enter company employee number: ")
 
@@ -49,8 +60,25 @@ def register_company():
     with open("DB\\RegisteredCompanies.txt", "a") as file:
         file.write(f"{name}#{email}#{password}#{license_no}#{employee_number}\n")
 
-    print("Company registered successfully!")
+    print("Registration successful!")
+    print(f"Your password is: {password}")
 
+def register_recruiter():
+    print("\n### Register as Recruiter ###")
+    email = input("Enter your email: ")
+    password = generate_random_password()
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    expertise_skills = input("Enter your expertise skills (comma-separated): ")
+    project_links = input("Enter your project links (comma-separated): ")
+    linkedin_link = input("Enter your LinkedIn link: ")
+
+    # Save recruiter information to file (RegisteredRecruiters.txt)
+    with open("DB\\RegisteredRecruiters.txt", "a") as file:
+        file.write(f"{email}#{password}#{name}#{age}#{expertise_skills}#{project_links}#{linkedin_link}\n")
+
+    print("Registration successful!")
+    print(f"Your password is: {password}")
 
 if __name__ == "__main__":
-    register_user()
+    execute()
---
+++ Utility.py
@@ -0,0 +1,34 @@
+import os
+
+def create_db_folder():
+    # Create a folder named 'DB' if it doesn't exist
+    if not os.path.exists("DB"):
+        os.makedirs("DB")
+
+def create_db_files():
+    # Create text files for the database if they don't exist
+    files = [
+        "DB\\RegisteredDevelopers.txt",
+        "DB\\RegisteredCompanies.txt",
+        "DB\\RegisteredRecruiters.txt",
+        "DB\\Admins.txt",
+        "DB\\AvailableJobs.txt",
+        "DB\\AppliedJobsOfDevelopers.txt",
+        "DB\\MessagesForDevelopers.txt",
+        "DB\\MessagesForRecruiters.txt",
+        "DB\\MessagesForCompanies.txt",
+        "DB\\AssignedRecruiters.txt",
+        "DB\\Interviews.txt"
+    ]
+
+    for file in files:
+        if not os.path.exists(file):
+            with open(file, "w"):
+                pass
+
+def execute():
+    create_db_folder()
+    create_db_files()
+
+if __name__ == "__main__":
+    execute()
